7e75206bf6bfcb003f324ff8232c873f477799ba
SinaSafari <m.sinasafari@gmail.com>
2020-10-25 17:26:17 +0330

    pca

--- src/Models.py
+++ src/Models.py
@@ -361,3 +361,34 @@ class RandomForest:
         tree_preds = np.swapaxes(tree_preds, 0, 1)
         y_pred = [most_common_label(tree_pred) for tree_pred in tree_preds]
         return np.array(y_pred)
+
+
+# pca
+class PCA:
+
+    def __init__(self, n_components):
+        self.n_components = n_components
+        self.components = None
+        self.mean = None
+
+    def fit(self, X):
+        # Mean centering
+        self.mean = np.mean(X, axis=0)
+        X = X - self.mean
+        # covariance, function needs samples as columns
+        cov = np.cov(X.T)
+        # eigenvalues, eigenvectors
+        eigenvalues, eigenvectors = np.linalg.eig(cov)
+        # -> eigenvector v = [:,i] column vector, transpose for easier calculations
+        # sort eigenvectors
+        eigenvectors = eigenvectors.T
+        idxs = np.argsort(eigenvalues)[::-1]
+        eigenvalues = eigenvalues[idxs]
+        eigenvectors = eigenvectors[idxs]
+        # store first n eigenvectors
+        self.components = eigenvectors[0:self.n_components]
+
+    def transform(self, X):
+        # project data
+        X = X - self.mean
+        return np.dot(X, self.components.T)
