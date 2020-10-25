cfef77cc3a2433d7ff8702bf91750471bd76524e
SinaSafari <m.sinasafari@gmail.com>
2020-10-25 16:57:22 +0330

    add base regression class

--- src/Models.py
+++ src/Models.py
@@ -1,28 +1,27 @@
 import numpy as np
 
 
-class LinearRegression():
+class BaseRegression():
     """
-    Linear Regression
+    abstract class parent of linear and logistic regression
     """
 
     def __init__(self, learning_rate=0.001, n_iters=1000):
         self.lr = learning_rate
         self.n_iters = n_iters
         self.weights = None
-        self.bias = None
-
+        self.bias = 0
 
     def fit(self, X, y):
         n_samples, n_features = X.shape
 
-        # parameters
+        # init parameters
         self.weights = np.zeros(n_features)
         self.bias = 0
 
         # gradient descent
         for _ in range(self.n_iters):
-            y_predicted = np.dot(X, self.weights) + self.bias
+            y_predicted = self._approximation(X, self.weights, self.bias)
 
             # compute gradients
             dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
@@ -32,7 +31,24 @@ class LinearRegression():
             self.weights -= self.lr * dw
             self.bias -= self.lr * db
 
-
     def predict(self, X):
-        y_approximated = np.dot(X, self.weights) + self.bias
-        return y_approximated
+        self._predict(X, self.weights, self.bias)
+
+    def _predict():
+        raise NotImplementedError()
+
+    def _approximation():
+        raise NotImplementedError()
+
+
+class LinearRegression(BaseRegression):
+
+    def _approximation():
+        pass
+
+    def _predict():
+        pass
+
+
+class LogisticRegression(BaseRegression):
+    pass
