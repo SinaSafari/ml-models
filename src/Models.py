3190c272020af906c59db2c446530abccb876e6e
SinaSafari <m.sinasafari@gmail.com>
2020-10-25 17:02:54 +0330

    logistic regression

--- src/Models.py
+++ src/Models.py
@@ -51,4 +51,16 @@ class LinearRegression(BaseRegression):
 
 
 class LogisticRegression(BaseRegression):
-    pass
+
+    def _sigmoid(self, x):
+        return 1 / (np.exp(-x) + 1)
+
+    def _approximation(self, X, w, b):
+        linear_model = np.dot(X, w) + b
+        return self._sigmoid(linear_model)
+
+    def _predict(self. X, w, b):
+        linear_model = np.dot(X, w) + b
+        y_predicted = self._sigmoid(linear_model)
+        y_predicted_cls = [1 if i > 0.5 else 0 for i in y_predicted]
+        return np.array(y_predicted_cls)
